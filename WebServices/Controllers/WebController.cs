897679161f33a864831c4e8dbd450d853c5c6351
Milan Milic <milan_milic@outlook.com>
2015-11-09 12:51:20 +0100

    https://github.com/milanmilic/WebServices.git

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation Get(int id)
+        {
+            return repo.Get(id);
+        }
+
+        public Reservation PostReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
