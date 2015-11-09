6a053d39307575414fbe502b2f1447e049a1ac8b
milanmilic <milan.milic@outlook.com>
2015-11-09 17:42:06 +0100

    zavrsena aplikacija (knockout dodat za dva diva i par dugmica). Aplikacija radi totalno kao Web API, bez mvc kontrolera

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -9,42 +9,10 @@ namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-        private ReservationRepository repo = ReservationRepository.Current;
-
         public ViewResult Index()
         {
-            return View(repo.GetAll());
+            return View();
         }
 
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                repo.Add(item);
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Index");
-            }
-        }
-
-        public ActionResult Remove(int id)
-        {
-            repo.Remove(id);
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && repo.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Index");
-            }
-        }
     }
 }
